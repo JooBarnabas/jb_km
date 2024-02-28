a27043f7135bc7ee4e04c98bfbcc5eebb9fb0633
JooBarnabas <joobarni@gmail.com>
2024-02-28 14:29:24 +0100

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <q-page>
+    <div class="justify-center" style="min-height: 200px">
+      <q-field class="row" label="Személyautó"></q-field>
+    </div>
+
+    <div class="row justify-center">
+      <q-card class="bg-blue-5 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+        Auto
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 3vw;
+}
+</style>
